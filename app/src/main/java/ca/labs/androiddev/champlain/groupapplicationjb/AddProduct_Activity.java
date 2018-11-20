9d4c5cfe3ed431690fda14a57fd8ad4efe04abe4
Matthew <grecoftw@gmail.com>
2018-11-20 10:34:22 -0500

    Project
    Added project class, updated mainview, updated DBManager and created view project activity

---
+++ app/src/main/java/ca/labs/androiddev/champlain/groupapplicationjb/AddProduct_Activity.java
@@ -0,0 +1,52 @@
+package ca.labs.androiddev.champlain.groupapplicationjb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import Models.DBManager;
+import Models.Product;
+
+public class AddProduct_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+
+
+    }
+
+    public void btnGoIndex_onClick(View v)
+    {
+        super.finish();
+    }
+
+    public void btnAdd_onClick(View v){
+        EditText nameInput = (EditText)findViewById(R.id.editName);
+        EditText priceInput = (EditText)findViewById(R.id.editPrice);
+        EditText descInput = (EditText)findViewById(R.id.editDescription);
+
+        String name = nameInput.getText().toString();
+        double price = Double.parseDouble(priceInput.getText().toString());
+        String desc = descInput.getText().toString();
+
+        DBManager db = new DBManager(this);
+
+     //   if(!db.productExists(new Product(name,price,desc))) {
+            db.addProduct(name, price, desc);
+            Toast.makeText(getApplication(), name + " added", Toast.LENGTH_LONG).show();
+
+            nameInput.setText("");
+            priceInput.setText("");
+            descInput.setText("");
+     //   }
+       /* else{
+            Toast.makeText(getApplication(), name + " already exists", Toast.LENGTH_LONG).show();
+        }*/
+
+    }
+}
--- app/src/main/java/ca/labs/androiddev/champlain/groupapplicationjb/MainActivity.java
+++ app/src/main/java/ca/labs/androiddev/champlain/groupapplicationjb/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
 import Models.User;
@@ -19,6 +19,11 @@ public class MainActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.btnAddProduct);
+      //  button.setEnabled(false);
+        button.setVisibility(View.GONE);
+    //    button.setBackgroundResource(R.drawable.ic_launcher_background);
     }
 
     @Override
@@ -28,6 +33,14 @@ public class MainActivity extends AppCompatActivity
         {
             TextView t = (TextView)findViewById(R.id.userTextView);
             t.setText("Welcome, " + User.getCurrUser());
+
+            if(User.getCurrUser().equals("admin")){
+                Button button = (Button)findViewById(R.id.btnAddProduct);
+              //  button.setEnabled(true);
+                button.setVisibility(View.VISIBLE);
+        }
+
+
         }
         super.onStart();
     }
@@ -46,6 +59,11 @@ public class MainActivity extends AppCompatActivity
     public void btnLogin_onClick(View v)
     {
         startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btnAddProduct_onClick(View v)
+    {
+        startActivity(new Intent(this, AddProduct_Activity.class));
     }
 
     public void btnRegister_onClick(View v)
